c1eef4d2de38f32b3384a99bfbd21a1a81fe3197
Kurbin Ademaj <62482850+kurbin-ademaj@users.noreply.github.com>
2021-03-21 12:45:13 +0100

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCD6OSfDGAc_FB5Chi0Wpw2JtvViEzWRYs",
+    authDomain: "netflix-original.firebaseapp.com",
+    projectId: "netflix-original",
+    storageBucket: "netflix-original.appspot.com",
+    messagingSenderId: "549140139777",
+    appId: "1:549140139777:web:d45777d653c92728771500"
+  };
